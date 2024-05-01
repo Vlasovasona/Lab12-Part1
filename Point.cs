e9eb1e9aa4d784a00e692b410de1588dc14565a8
Sofa Vlasova <147415088+Vlasovasona@users.noreply.github.com>
2024-05-01 19:12:23 +0500

    Update Point.cs

--- Point.cs
+++ Point.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using Library_10;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
@@ -7,11 +8,25 @@ using System.Threading.Tasks;
 
 namespace Лаба12_часть1
 {
-    public class Point<T>
+    public class Point<T> where T: IInit, new()
     {
         public T? Data { get; set; }
         public Point<T>? Next {  get; set; }
         public Point<T>? Pred { get; set; }
+
+        public static Point<T> MakeRandomData() //создание элемента типа <T>
+        {
+            T data = new T(); //должно иметь конструктор без параметров 
+            data.RandomInit(); //рандомно заполняем 
+            return new Point<T>(data); //создаем новый элемент 
+        }
+
+        public static T MakeRandomItem() //создание информационного поля
+        {
+            T data = new T();
+            data.RandomInit();
+            return data;
+        }
 
         public Point()
         {
@@ -19,19 +34,17 @@ namespace Лаба12_часть1
             this.Next = null;
             this.Pred = null;
         }
+
         public Point(T data)
         {
             this.Data = data;
             this.Next = null;
             this.Pred = null;
         }
+
         public override string ToString() //преобразование элемента типа Point в строку 
         {
             return Data == null ? "" : Data.ToString(); //проверка на null (если Data пустая, будет возвращена пустая строка)
-        }
-        public override int GetHashCode() //ананлогично работает метод GetHashCode (нужен, т.к. работаем с HashTable)
-        {
-            return Data == null ? 0 : Data.GetHashCode();
         }
     }
 }
